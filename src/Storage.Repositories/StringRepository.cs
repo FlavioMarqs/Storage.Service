ad5d2bdd82df85395f18419584a7ddf758215478
FlavioMarqs <flaviofmmarques@gmail.com>
2023-09-09 18:50:17 +0200

    Adding Storage.Repositories project with IRepository interface and early StringRepository implementation

---
+++ src/Storage.Repositories/StringRepository.cs
@@ -0,0 +1,32 @@
+﻿using Storage.Repositories.Interfaces;
+
+namespace Storage.Repositories
+{
+    public class StringRepository : IRepository<string>
+    {
+        public Task CreateAsync(string entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(string entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
