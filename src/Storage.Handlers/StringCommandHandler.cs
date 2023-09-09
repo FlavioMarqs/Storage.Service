0a59cda68ccd5e840f7d50d84e5fa0c57ec05578
FlavioMarqs <flaviofmmarques@gmail.com>
2023-09-09 18:49:43 +0200

    Adding Storage.Handlers project, ICommandHandler interface and first vague implementation

---
+++ src/Storage.Handlers/StringCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Storage.Handlers.Interfaces;
+using System.Windows.Input;
+
+namespace Storage.Handlers
+{
+    public class StringCommandHandler : ICommandHandler
+    {
+        public StringCommandHandler(IRepository<string> repo) 
+        { 
+        }
+
+        public Task HandleAsync(ICommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
