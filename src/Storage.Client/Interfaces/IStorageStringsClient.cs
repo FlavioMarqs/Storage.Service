38d487493011fc77e08786b4aeaf8a9895bce585
FlavioMarqs <flaviofmmarques@gmail.com>
2023-09-10 17:40:39 +0200

    Early implementation of Storage.Client for strings for the Storage.Service

---
+++ src/Storage.Client/Interfaces/IStorageStringsClient.cs
@@ -0,0 +1,16 @@
+﻿using Storage.DTOs.Requests;
+using Storage.DTOs.Responses;
+
+namespace Storage.Client.Interfaces
+{
+    public interface IStorageStringsClient
+    {
+        Task<HttpResponseMessage> PutStringAsync(StringCreationRequest request);
+
+        Task<HttpResponseMessage> GetStringByIdAsync(StringQueryRequest request);
+
+        Task<HttpResponseMessage> GetAllStringsAsync(StringsQueryRequest request);
+
+        Task<HttpResponseMessage> DeleteStringAsync(StringRemovalRequest request);
+    }
+}
---
+++ src/Storage.Client/StorageClientOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Client
+{
+    public class StorageClientOptions
+    {
+        private const string _stringsControllerPath = "Storage/Strings";
+        public string ApiServiceUrl { get; set; }
+
+        public string ApiServiceUrlWithSlash => $"{ApiServiceUrl.TrimEnd('/')}/";
+
+        public string StorageStringsUrl => $"{ApiServiceUrlWithSlash}{_stringsControllerPath}";
+    }
+}
