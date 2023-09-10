49ee04c1ffbb4b64f0c8da6adc77fbdcdd850e5e
FlavioMarqs <flaviofmmarques@gmail.com>
2023-09-10 15:22:29 +0200

    Refactoring of StorageController; added endpoint to retrieve a given String object based on its identifier

--- src/Storage.Service/Controllers/StorageController.cs
+++ src/Storage.Service/Controllers/StorageController.cs
@@ -1,6 +1,10 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Storage.Commands;
+using Storage.Commands.Commands;
+using Storage.Commands.Queries;
+using Storage.Domain;
 using Storage.Handlers.Interfaces;
+using Storage.Service.Requests;
 using System.Net;
 
 namespace WebApplication1.Controllers
@@ -9,35 +13,58 @@ namespace WebApplication1.Controllers
     [Route("[controller]")]
     public class StorageController : ControllerBase
     {
-        private ICommandHandler<StringStoreCommand> _stringHandler;
-
+        private readonly ICommandHandler<StringStoreCommand, StringDTO> _stringHandler;
+        private readonly ICommandHandler<StringQueryCommand, StringDTO> _stringQueryHandler;
+        private readonly IMapper _mapper;
         private readonly ILogger<StorageController> _logger;
 
-        public StorageController(ILogger<StorageController> logger, ICommandHandler<StringStoreCommand> stringHandler)
+        public StorageController(ILogger<StorageController> logger, ICommandHandler<StringStoreCommand, StringDTO> stringHandler, IMapper mapper)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _stringHandler = stringHandler ?? throw new ArgumentNullException(nameof(stringHandler));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
-        [HttpPut(Name = "Strings")]
-        public async Task<HttpStatusCode> Put(StringStoreCommand command)
+        [HttpPut(Name = "String")]
+        public async Task<IActionResult> Put(StringCreationRequest request)
         {
-            if(string.IsNullOrWhiteSpace(command.Value))
-                return HttpStatusCode.BadRequest;
+            if(string.IsNullOrWhiteSpace(request.Value))
+                return BadRequest("Invalid value submitted; cannot be null, empty or whitespaces.");
 
             _logger.LogInformation($"Storing string at {DateTime.UtcNow}.");
             
             try
             {
+                var command = _mapper.Map<StringStoreCommand>(request);
                 await _stringHandler.HandleAsync(command);
             }
             catch (Exception ex)
             {
                 _logger.LogError($"Something went wrong when submitting a new StringStoreCommand", ex);
-                return HttpStatusCode.Conflict;
+                return Conflict(ex.GetType());
             }
 
-            return HttpStatusCode.OK;
+            return Ok();
+        }
+
+        [HttpGet(Name = "Strings")]
+        public async Task<IActionResult> Get(StringQueryRequest request)
+        {
+            if (request.Identifier < 1)
+                return BadRequest("Invalid value submitted; must be above zero.");
+
+            _logger.LogInformation($"Querying for String with identifier {request.Identifier}");
+            try
+            {
+                var command = _mapper.Map<StringQueryCommand>(request);
+                await _stringQueryHandler.HandleAsync(command);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong when submitting a new StringStoreCommand", ex);
+                return NotFound(request.Identifier);
+            }
+            return Ok();
         }
     }
 }
