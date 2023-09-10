0b042c04ef5bef3b8499eef1e9052aa60141466d
FlavioMarqs <flaviofmmarques@gmail.com>
2023-09-10 15:17:33 +0200

    Added Domain/DTO project (TODO: consider renaming to Storage.DTOs)

---
+++ src/Storage.Domain/StringDTO.cs
@@ -0,0 +1,15 @@
+﻿using Storage.Domain.Interfaces;
+
+namespace Storage.Domain
+{
+    public class StringDTO : IDTO
+    {
+        public int Identifier { get; set; }
+
+        public string StringValue { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? LastModifiedAt { get; set; }
+    }
+}
