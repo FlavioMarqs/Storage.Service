447edcd9826ea4684eaaa5daf57415132bed8f72
FlavioMarqs <flaviofmmarques@gmail.com>
2023-09-10 15:21:23 +0200

    Renamed Setup to Startup Added new dependencies in paket

---
+++ src/Storage.Service/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Storage.Repositories;
+using Storage.Handlers;
+
+namespace Storage.Service
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        public IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            services.AddRepositories(_config);
+            services.AddMvc().AddXmlSerializerFormatters();
+            services.AddHandlers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            return services;
+        }
+    }
+}
