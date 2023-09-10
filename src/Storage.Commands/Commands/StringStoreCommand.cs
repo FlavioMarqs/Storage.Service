26a5447c0b21baba9fafacf05a7ad7315fce36e0
FlavioMarqs <flaviofmmarques@gmail.com>
2023-09-10 14:55:04 +0200

    Refactored Storage.Commands to match expected operations

---
+++ src/Storage.Commands/Commands/StringStoreCommand.cs
@@ -0,0 +1,9 @@
+﻿using Storage.Commands.Interfaces;
+
+namespace Storage.Commands.Commands
+{
+    public class StringStoreCommand : IStoreCommand<string>
+    {
+        public string Value { get; set; }
+    }
+}
