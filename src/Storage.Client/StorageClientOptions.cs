e11bc63f6562221a2f6f532a9b51ab3858970278
FlavioMarqs <flaviofmmarques@gmail.com>
2023-09-10 18:48:03 +0200

    Removing unused imports

--- src/Storage.Client/StorageClientOptions.cs
+++ src/Storage.Client/StorageClientOptions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Storage.Client
+﻿namespace Storage.Client
 {
     public class StorageClientOptions
     {
